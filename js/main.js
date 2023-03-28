5431195d688881ae17786327c24211035b225b7d
Gabriele31201 <gabri31201@gmail.com>
2023-03-28 16:56:03 +0200

    add password generator

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-const nome=prompt("Inserisci il tuo nome:");
-const cognome=prompt("Inserisci il tuo cognome:");
-const colore=prompt("Inserisci il tuo colore preferito:");
+const nome = prompt("Inserisci il tuo nome:");
+const cognome = prompt("Inserisci il tuo cognome:");
+const colore = prompt("Inserisci il tuo colore preferito:");
+let password = (nome+cognome+colore+21)
 
 
-let passwordGeneretionWannabe='
+let passwordGeneretorWannabe =`
 ---------- Generatore Password ---------- 
 > Informazioni
 
-Gabriele ${nome}
-Simone  ${cognome}
-';
+nome: ${nome}
+cognome: ${cognome}
+colore: ${colore}
+
+---------- Password Generata ----------
+
+password: ${password}
+`;
 
 
 
-console.log(passwordGeneretionWannabe);
+
+console.log(passwordGeneretorWannabe);
