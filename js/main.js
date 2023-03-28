8855c2a476f0fb461aeae454dbb2d69615e63088
Gabriele31201 <gabri31201@gmail.com>
2023-03-28 16:29:25 +0200

    add info js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nome=prompt("Inserisci il tuo nome:");
+const cognome=prompt("Inserisci il tuo cognome:");
+const colore=prompt("Inserisci il tuo colore preferito:");
+
+
+let passwordGeneretionWannabe='
+---------- Generatore Password ---------- 
+> Informazioni
+
+Gabriele ${nome}
+Simone  ${cognome}
+';
+
+
+
+console.log(passwordGeneretionWannabe);
